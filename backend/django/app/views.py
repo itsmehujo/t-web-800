5711efb5c42401d5f07e4ae34710c355465541dd
Hugo Mortreux <70602545+itsmehujo@users.noreply.github.com>
2023-02-20 11:50:59 +0100

    update run and dockerfile for symfony

--- backend/django/app/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-
-def index(request):
-
-    data = {
-        'message': "Hello from the backend"
-    }
-
-    return JsonResponse(data)
--- backend/django/tests/test_ci.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def inc(x):
-    return x + 1
-
-
-def test_should_work():
-    assert inc(3) == 4
